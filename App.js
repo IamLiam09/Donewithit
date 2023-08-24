d442141554ef5238affb2aa823c66775a1da0647
Ndubuisi William <princewill835@gmail.com>
2023-08-24 02:43:53 -0700

    js debugger

--- App.js
+++ App.js
@@ -1,22 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
-  let x = 1;
-  console.log("App executed")
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working o</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	console.log("App react native executed");
+return (
+		<View style={styles.container}>
+			<Text>Open up App.js to start working o</Text>
+			<StatusBar style="auto" />
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: "#fff",
+		alignItems: "center",
+		justifyContent: "center",
+	},
 });
